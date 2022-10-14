9d9cefce42aa5486cc28edbd81eb0cdb481190ea
Rajdeep Sarkar (Intern) <rajdeeps@MPTDC1-India.MirrorPlus.com>
2022-10-14 14:08:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferenceexample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.sharedpreferenceexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    Button savebtn,showbtn;
+    ArrayList al = new ArrayList();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.putdata);
+        savebtn = (Button) findViewById(R.id.savedata);
+        showbtn = (Button) findViewById(R.id.viewdata);
+        textView = (TextView) findViewById(R.id.showdata);
+
+
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sp = getSharedPreferences("text", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("Name",editText.getText().toString());
+                editor.apply();
+                editText.setText("");
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+        showbtn.setOnClickListener(new View.OnClickListener() {
+            SharedPreferences sp = getSharedPreferences("text", MODE_PRIVATE);
+            @Override
+            public void onClick(View view) {
+                String s = sp.getString("Name", "");
+                textView.setText(s);
+            }
+        });
+    }
+}
